a6dd88f87119bd1748481e2c85ca33b4b807cff2
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-28 23:05:21 +0900

    画像とメソッドの配置

--- SwiftTimer1/ViewController.swift
+++ SwiftTimer1/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var ImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func start(_ sender: Any) {
+    
+  }
+  
+  
+  @IBAction func stop(_ sender: Any) {
+    
+  }
 }
 
