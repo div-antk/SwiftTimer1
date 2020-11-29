cf57fa05a85f642c2239450487caf560b0946af8
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-29 22:58:15 +0900

    カウントの処理と空配列の作成

--- SwiftTimer1/ViewController.swift
+++ SwiftTimer1/ViewController.swift
@@ -13,14 +13,32 @@ class ViewController: UIViewController {
   @IBOutlet weak var startButton: UIButton!
   @IBOutlet weak var stopButton: UIButton!
   
+  var timer = Timer()
+  var count = Int()
+  // 画像の配列を宣言
+  var imageArray = [UIImage]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    count = 0
     // ストップボタンを押せなくする
     stopButton.isEnabled = true
     
   }
+  
+  func startTimer() {
+    // タイマーを回す。0.2秒毎にメソッドを呼ぶ
+    timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+  }
 
+  @objc func timerUpdate() {
+    // 0.2秒毎に呼ばれる
+    count = count + 1
+    
+    ImageView.image = imageArray[count]
+  }
+  
   @IBAction func start(_ sender: Any) {
     
     startButton.isEnabled = true
